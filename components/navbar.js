8f00ba05bdd8e77cd76800eb6947d922deef88b1
Manish Kumar <babitakumari20051999@gmail.com>
2022-01-21 10:03:44 +0530

    ....

--- components/navbar.js
+++ components/navbar.js
@@ -6,8 +6,8 @@ function navbar(){
   
         <ul>
            <a href="index.html"><li>HOME</li></a>
-          <a href=""><li>ABOUT US</li></a>
-           <a href=""><li>OUR MENU</li></a>
+          <a href="locate.html"><li>ABOUT US</li></a>
+           <a href="ourmenu.html"><li>OUR MENU</li></a>
          <a href="campaigns.html"><li>CAMPAIGNS</li></a>
           <a href="locate.html"><li>LOCATE US</li></a>
            <a href="https://bit.ly/3xXzxJ6"><li>ORDER NOW</li></a>
