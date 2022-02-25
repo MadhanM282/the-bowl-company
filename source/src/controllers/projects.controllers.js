b696cad1952cb38f828757e5738dbe9500d45795
MadhanM282 <madhanm282@gmail.com>
2022-02-25 07:36:29 +0530

    menu

---
+++ source/src/controllers/projects.controllers.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const Food = require("../models/projects.models");
+
+const app = express();
+
+
+const router = express.Router();
+
+// make router for frontend
+
+// posting food
+router.post("", async (req, res) => {
+    try {
+      const food = await Food.create(req.body);
+      res.send(food);
+    }
+    catch (err) {
+      res.send(err.message);
+    }
+})
+
+// getting food
+router.get("",async (req, res) => {
+  try {
+    const count = req.query.count;
+    // console.log('count', count);
+    const size = count*9
+    const food = await Food.find().limit(size).lean().exec();
+    return res.send(food);
+    // res.send(food);
+  } catch (err) {
+    res.status(520).send(err.message);
+  }
+});
+
+router.patch("/:id", async (req, res)=> {
+  try {
+    const food = await Food.findByIdAndUpdate(req.params.id,req.body)
+    res.send(food);
+  } catch (err) {
+    console.log('err', err);
+  }
+})
+
+module.exports = router;
