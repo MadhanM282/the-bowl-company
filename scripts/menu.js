710c529794709d1aceaaa2343d63bb7e5f08c4fb
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-02-25 11:01:41 +0530

    done

--- scripts/menu.js
+++ scripts/menu.js
@@ -9,7 +9,7 @@ async function searching(){
         let data = await res.json();
         console.log('data', data);   
         // return data.meals;
-        display(data)
+        display(data);
     }
     catch(e){
         console.log('e', e);
@@ -30,73 +30,70 @@ async function category(i){
         let data = await res.json();
         console.log('data', data);   
         // return data.meals;
-        display(data)
+        display(data);
     }
     catch(e){
         console.log('e', e);
     }
 }
 
-
-
-
 function display(data){
-    let main = document.querySelector("#fithdiv")
-    main.innerHTML = null
-    let arr = []
+    let main = document.querySelector("#fithdiv");
+    main.innerHTML = null;
+    let arr = [];
     data.map((ele)=>{
         let div = document.createElement('div');
         let div1 = document.createElement('div');
-        div1.setAttribute('class', 'image')
+        div1.setAttribute('class', 'image');
 
         let img = document.createElement('img');
         img.setAttribute('class', 'image__img');
         img.src = ele.image;
 
         let div2 = document.createElement('div');
-        div2.setAttribute('class', 'image__overlay image__overlay--primary')
+        div2.setAttribute('class', 'image__overlay image__overlay--primary');
         // div2.setAttribute('class', );
 
         let p = document.createElement('p');
-        p.setAttribute('class', 'image__description')
-        p.innerHTML = ele.description
+        p.setAttribute('class', 'image__description');
+        p.innerHTML = ele.description;
 
         div2.append(p);
 
 
         let name = document.createElement('h3');
-        name.setAttribute("class","downname")
+        name.setAttribute("class", "downname");
         name.innerHTML= ele.name;
 
         let svg = document.createElement('img');
         if(ele.type === "veg"){
-            svg.src = "https://img.icons8.com/ios-glyphs/30/26e07f/non-vegetarian-food-symbol.png"  
+            svg.src = "https://img.icons8.com/ios-glyphs/30/26e07f/non-vegetarian-food-symbol.png";  
         }
         else if(ele.type === "nonveg"){
-           svg.src="https://img.icons8.com/ios-glyphs/30/ed1c24/non-vegetarian-food-symbol.png"
+            svg.src = "https://img.icons8.com/ios-glyphs/30/ed1c24/non-vegetarian-food-symbol.png";
 
         }
-        let price = document.createElement('h4')
-        price.innerHTML= `₹${ele.price}`
+        let price = document.createElement('h4');
+        price.innerHTML = `₹${ele.price}/-`;
         let button = document.createElement('button');
 
         button.addEventListener('click',cart);
 
         function cart(){
-            arr.push(ele)
+            arr.push(ele);
             localStorage.setItem('cart', JSON.stringify(arr));
         }
 
-        button.innerHTML= "Add+"
+        button.innerHTML = "Add+";
 
         const namediv = document.createElement('div');
-        namediv.setAttribute('class', 'namediv')
+        namediv.setAttribute('class', 'namediv');
 
-        namediv.append(price,button)
+        namediv.append(price, button);
         // div3.append(name);
 
         div1.append(img,div2);
-        div.append(div1,svg,name,namediv)
+        div.append(div1, svg, name, namediv);
 
         main.append(div);
         // console.log('ele',ele)
@@ -104,4 +101,4 @@ function display(data){
 }
 
 
-searching()
+searching();
