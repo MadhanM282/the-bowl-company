78aa5ce777d9b962bcda734108b35939c13abc8e
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-02-26 14:06:44 +0530

    r

--- source/src/configs/google-oauth.js
+++ source/src/configs/google-oauth.js
@@ -4,12 +4,12 @@ const GoogleStrategy = require("passport-google-oauth2").Strategy;
 const { v4: uuidv4 } = require("uuid");
 
 const User = require("../models/user.model");
-
+// process.env.GOOGLE_CLIENT_IDprocess.env.GOOGLE_CLIENT_SECRET
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientID:"185288388593-ccb2rhaf3aus46ippr28g835ru68fad6.apps.googleusercontent.com" ,
+      clientSecret:"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" ,
       callbackURL: "http://localhost:2345/auth/google/callback",
       passReqToCallback: true,
     },
